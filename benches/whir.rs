da79c9c6a26f8aa841f13695d5aa28df2262de55
Thomas Coratger <thomas.coratger@gmail.com>
2025-04-02 18:02:17 +0200

    bench touchups

--- benches/whir.rs
+++ benches/whir.rs
@@ -5,7 +5,6 @@ use whir_p3::{
 };
 
 fn benchmark_whir(c: &mut Criterion) {
-    // Choose the worst-case parameters for benchmarking
     let num_variables = 12;
     let folding_factor = FoldingFactor::Constant(4);
     let num_points = 2;
@@ -13,7 +12,7 @@ fn benchmark_whir(c: &mut Criterion) {
     let pow_bits = 10;
     let fold_type = FoldType::ProverHelps;
 
-    c.bench_function("whir_end_to_end_worst_case", |b| {
+    c.bench_function("whir_end_to_end", |b| {
         b.iter(|| {
             make_whir_things(
                 num_variables,
