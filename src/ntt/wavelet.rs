66bbd25f512793bc23384d4882f1f0bf080a773b
AngusG <angusgruen@gmail.com>
2025-05-16 14:41:19 -0400

    Using packings and better parallelism.

--- src/ntt/wavelet.rs
+++ src/ntt/wavelet.rs
@@ -1,5 +1,6 @@
-use p3_field::Field;
-use p3_matrix::{Matrix, dense::RowMajorMatrixViewMut};
+use p3_field::{Field, PackedValue};
+use p3_matrix::{Matrix, dense::RowMajorMatrixViewMut, util::reverse_matrix_index_bits};
+use p3_util::log2_strict_usize;
 #[cfg(feature = "parallel")]
 use {super::utils::workload_size, rayon::prelude::*};
 
@@ -131,88 +132,43 @@ pub fn inverse_wavelet_transform_batch<F: Field>(
 /// Assumes the number of rows is a power of two.
 pub fn wavelet_transform<F: Field>(mat: &mut RowMajorMatrixViewMut<'_, F>) {
     let height = mat.height();
-    debug_assert!(height.is_power_of_two());
-    wavelet_transform_batch(mat, height);
+    let log_height = log2_strict_usize(height);
+    let half_log = log_height / 2;
+
+    // Start by reversing the matrix indices.
+    reverse_matrix_index_bits(mat);
+
+    for i in 0..half_log {
+        let block_size = 1 << (log_height - i);
+
+        // Apply the wavelet kernel on blocks of size `block_size`.
+        wavelet_kernel(mat, block_size);
+    }
+
+    // Start by reversing the matrix indices.
+    reverse_matrix_index_bits(mat);
+    for i in half_log..log_height {
+        let block_size = 1 << (i + 1);
+
+        // Apply the wavelet kernel on blocks of size `block_size`.
+        wavelet_kernel(mat, block_size);
+    }
 }
 
-pub fn wavelet_transform_batch<F: Field>(mat: &mut RowMajorMatrixViewMut<'_, F>, size: usize) {
-    debug_assert!(mat.height() % size == 0 && size.is_power_of_two());
-
-    #[cfg(feature = "parallel")]
-    if mat.height() > workload_size::<F>() && mat.height() != size {
-        let chunk_rows = size * std::cmp::max(1, workload_size::<F>() / size);
-        mat.par_row_chunks_mut(chunk_rows)
-            .for_each(|mut chunk| wavelet_transform_batch(&mut chunk, size));
-        return;
-    }
-
-    match size {
-        0 | 1 => {}
-        2 => {
-            for v in mat.row_chunks_exact_mut(2) {
-                v.values[1] += v.values[0];
-            }
-        }
-        4 => {
-            for v in mat.row_chunks_exact_mut(4) {
-                v.values[1] += v.values[0];
-                v.values[3] += v.values[2];
-                v.values[2] += v.values[0];
-                v.values[3] += v.values[1];
-            }
-        }
-        8 => {
-            for v in mat.row_chunks_exact_mut(8) {
-                v.values[1] += v.values[0];
-                v.values[3] += v.values[2];
-                v.values[2] += v.values[0];
-                v.values[3] += v.values[1];
-                v.values[5] += v.values[4];
-                v.values[7] += v.values[6];
-                v.values[6] += v.values[4];
-                v.values[7] += v.values[5];
-                v.values[4] += v.values[0];
-                v.values[5] += v.values[1];
-                v.values[6] += v.values[2];
-                v.values[7] += v.values[3];
-            }
-        }
-        16 => {
-            for mut v in mat.row_chunks_exact_mut(16) {
-                for v in v.row_chunks_exact_mut(4) {
-                    v.values[1] += v.values[0];
-                    v.values[3] += v.values[2];
-                    v.values[2] += v.values[0];
-                    v.values[3] += v.values[1];
-                }
-                let (a, mut v) = v.split_rows_mut(4);
-                let (b, mut v) = v.split_rows_mut(4);
-                let (c, d) = v.split_rows_mut(4);
-                for i in 0..4 {
-                    b.values[i] += a.values[i];
-                    d.values[i] += c.values[i];
-                    c.values[i] += a.values[i];
-                    d.values[i] += b.values[i];
-                }
-            }
-        }
-        n => {
-            let n1 = 1 << (n.trailing_zeros() / 2);
-            let n2 = size / n1;
-
-            wavelet_transform_batch(mat, n1);
-            mat.par_row_chunks_exact_mut(n1 * n2).for_each(|matrix| {
-                let m = RowMajorMatrixViewMut::new(matrix.values, n1).transpose();
-                matrix.values.copy_from_slice(&m.values);
-            });
-
-            wavelet_transform_batch(mat, n2);
-            mat.par_row_chunks_exact_mut(n1 * n2).for_each(|matrix| {
-                let m = RowMajorMatrixViewMut::new(matrix.values, n2).transpose();
-                matrix.values.copy_from_slice(&m.values);
-            });
-        }
-    }
+/// Apply the wavelet kernel on blocks of a given size.
+fn wavelet_kernel<F: Field>(mat: &mut RowMajorMatrixViewMut<'_, F>, block_size: usize) {
+    let half_block_size = block_size / 2;
+    mat.par_row_chunks_exact_mut(block_size).for_each(|block| {
+        let (lo, hi) = block.values.split_at_mut(half_block_size);
+        let (pack_lo, sfx_lo) = F::Packing::pack_slice_with_suffix_mut(lo);
+        let (pack_hi, sfx_hi) = F::Packing::pack_slice_with_suffix_mut(hi);
+        pack_hi.iter_mut().zip(pack_lo).for_each(|(hi, lo)| {
+            *hi += *lo;
+        });
+        sfx_hi.iter_mut().zip(sfx_lo).for_each(|(hi, lo)| {
+            *hi += *lo;
+        });
+    });
 }
 
 #[cfg(test)]
@@ -366,56 +322,6 @@ mod tests {
         // Verify last element has accumulated all previous values
         let expected_last = (1..=size).sum::<u64>();
         assert_eq!(mat.values[size as usize - 1], F::from_u64(expected_last));
-    }
-
-    #[test]
-    fn test_wavelet_transform_batch_parallel_chunks() {
-        // Define the size for the wavelet transform batch
-        let batch_size = 2_i32.pow(20) as usize;
-        // Ensure values.len() > size to enter parallel execution
-        let total_size = batch_size * 4;
-        let values = (1..=total_size as u64).map(F::from_u64).collect::<Vec<_>>();
-
-        // Keep a copy to compare later
-        let original_values = values.clone();
-
-        let mut mat = RowMajorMatrix::new_col(values);
-
-        // Run batch transform on 256-sized chunks
-        wavelet_transform_batch(&mut mat.as_view_mut(), batch_size);
-
-        // Verify that the first chunk has been transformed correctly
-        let mut mat1 = RowMajorMatrix::new_col(original_values[..batch_size].to_vec());
-
-        wavelet_transform_batch(&mut mat1.as_view_mut(), batch_size);
-        assert_eq!(mat.values[..batch_size], mat1.values);
-
-        // Ensure that the transformation occurred separately for each chunk
-        for i in 1..4 {
-            let start = i * batch_size;
-            let end = start + batch_size;
-
-            let mut mat_loop = RowMajorMatrix::new_col(original_values[start..end].to_vec());
-            wavelet_transform_batch(&mut mat_loop.as_view_mut(), batch_size);
-
-            assert_eq!(
-                mat.values[start..end],
-                mat_loop.values,
-                "Mismatch in chunk {i}"
-            );
-        }
-
-        // Ensure the first element remains unchanged
-        assert_eq!(mat.values[0], F::from_u64(1));
-
-        // Ensure the last element has accumulated all values from its own chunk
-        let expected_last_chunk_sum =
-            (total_size as u64 - batch_size as u64 + 1..=total_size as u64).sum::<u64>();
-        assert_eq!(
-            mat.values[total_size - 1],
-            F::from_u64(expected_last_chunk_sum),
-            "Final element mismatch"
-        );
     }
 
     #[test]
