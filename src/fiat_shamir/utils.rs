9bed8a31e6f415017a317d1bfcb388405ba4f71d
Thomas Coratger <thomas.coratger@gmail.com>
2025-05-26 20:46:08 +0200

    fiat shamir: rm useless bytes_modp fn

--- src/fiat_shamir/utils.rs
+++ src/fiat_shamir/utils.rs
@@ -27,11 +27,6 @@ pub(crate) fn from_le_bytes_mod_order<F: PrimeField64>(bytes: &[u8]) -> F {
     F::from_u64(reduced.try_into().unwrap())
 }
 
-/// Bytes needed in order to encode an element of F.
-pub(crate) const fn bytes_modp(modulus_bits: u32) -> usize {
-    (modulus_bits as usize).div_ceil(8)
-}
-
 /// Bytes needed in order to obtain a uniformly distributed random element of `modulus_bits`
 pub(crate) const fn bytes_uniform_modp(modulus_bits: u32) -> usize {
     (modulus_bits as usize + 128) / 8
