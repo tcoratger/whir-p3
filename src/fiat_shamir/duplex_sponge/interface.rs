6faf51c46f466b11b128b0f5c7236d0c823f0e03
Thomas Coratger <thomas.coratger@gmail.com>
2025-05-29 12:28:33 +0200

    fiat shamir: add Unit back to have generic

--- src/fiat_shamir/duplex_sponge/interface.rs
+++ src/fiat_shamir/duplex_sponge/interface.rs
@@ -9,13 +9,37 @@ use p3_symmetric::Permutation;
 ///
 /// **HAZARD**: Don't implement this trait unless you know what you are doing.
 /// Consider using the sponges already provided by this library.
-pub trait DuplexSpongeInterface<C: Permutation<[u8; 200]>>: zeroize::Zeroize {
+pub trait DuplexSpongeInterface<C: Permutation<[u8; 200]>, U = u8>: zeroize::Zeroize
+where
+    U: Unit,
+{
     /// Initializes a new sponge, setting up the state.
     fn new(permutation: C, iv: [u8; 32]) -> Self;
 
     /// Absorbs new elements in the sponge.
-    fn absorb_unchecked(&mut self, input: &[u8]) -> &mut Self;
+    fn absorb_unchecked(&mut self, input: &[U]) -> &mut Self;
 
     /// Squeezes out new elements.
-    fn squeeze_unchecked(&mut self, output: &mut [u8]) -> &mut Self;
+    fn squeeze_unchecked(&mut self, output: &mut [U]) -> &mut Self;
+}
+
+/// Basic units over which a sponge operates.
+///
+/// We require the units to have a precise size in memory, to be cloneable,
+/// and that we can zeroize them.
+pub trait Unit: Clone + Sized + zeroize::Zeroize {
+    /// Write a bunch of units in the wire.
+    fn write(bunch: &[Self], w: &mut impl std::io::Write) -> Result<(), std::io::Error>;
+    /// Read a bunch of units from the wire
+    fn read(r: &mut impl std::io::Read, bunch: &mut [Self]) -> Result<(), std::io::Error>;
+}
+
+impl Unit for u8 {
+    fn write(bunch: &[Self], w: &mut impl std::io::Write) -> Result<(), std::io::Error> {
+        w.write_all(bunch)
+    }
+
+    fn read(r: &mut impl std::io::Read, bunch: &mut [Self]) -> Result<(), std::io::Error> {
+        r.read_exact(bunch)
+    }
 }
