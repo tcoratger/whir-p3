fc8903075c5244ed0257129110db35154363d9f5
Thomas Coratger <thomas.coratger@gmail.com>
2025-07-22 13:50:40 +0200

    sumcheck: small touchups

--- src/sumcheck/sumcheck_single.rs
+++ src/sumcheck/sumcheck_single.rs
@@ -143,14 +143,15 @@ where
     prover_state.pow_grinding(pow_bits);
 
     // Compress polynomials and update the sum.
-
     #[cfg(feature = "parallel")]
     let evals = { rayon::join(|| compress(weights, r), || compress_ext(evals, r)).1 };
+
     #[cfg(not(feature = "parallel"))]
     let evals = {
         compress(weights, r);
         compress_ext(evals, r)
     };
+
     *sum = sumcheck_poly.evaluate_at_point(&r.into());
 
     (r, evals)
@@ -193,15 +194,16 @@ where
 
     // Compress polynomials and update the sum.
     #[cfg(feature = "parallel")]
-    {
-        rayon::join(|| compress(evals, r), || compress(weights, r));
-    }
+    rayon::join(|| compress(evals, r), || compress(weights, r));
+
     #[cfg(not(feature = "parallel"))]
     {
         compress(evals, r);
         compress(weights, r);
     }
+
     *sum = sumcheck_poly.evaluate_at_point(&r.into());
+
     r
 }
 
