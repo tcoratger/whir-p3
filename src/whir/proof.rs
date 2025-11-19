77e6d17c8927e80e682f0bb5a8430e6317a94ce8
Carlo Modica <92799914+CarloModicaPortfolio@users.noreply.github.com>
2025-11-19 15:06:26 +0100

    Update src/whir/proof.rs
    Co-authored-by: Thomas Coratger <60488569+tcoratger@users.noreply.github.com>

--- src/whir/proof.rs
+++ src/whir/proof.rs
@@ -173,14 +173,6 @@ pub struct SumcheckData<EF, F> {
     pub pow_witnesses: Option<Vec<F>>,
 }
 
-impl<EF: Default, F: Default> Default for SumcheckData<EF, F> {
-    fn default() -> Self {
-        Self {
-            polynomial_evaluations: Vec::new(),
-            pow_witnesses: None,
-        }
-    }
-}
 
 impl<F: Default, EF: Default, const DIGEST_ELEMS: usize> WhirProof<F, EF, DIGEST_ELEMS> {
     /// Create a new WhirProof from protocol parameters and configuration
